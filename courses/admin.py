9f98606ac5a27e7b8bdb22e3911c7dec06125134
Shariq Farooq <shariqfarooq921@gmail.com>
2025-08-09 20:04:11 +0500

    .

--- courses/admin.py
+++ courses/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
-# Import the models that actually exist in your models.py file
-from .models import Course, Lesson, UserProgress
+# Import all the models you want to see in the admin panel
+from .models import Course, Lesson, UserProgress, Quiz, Question, Answer, QuizAttempt
 
-# Register your models here so you can manage them in the Django admin panel.
+# Register your models here.
 admin.site.register(Course)
 admin.site.register(Lesson)
 admin.site.register(UserProgress)
+
+# Add these lines to register the new quiz models
+admin.site.register(Quiz)
+admin.site.register(Question)
+admin.site.register(Answer)
+admin.site.register(QuizAttempt)
